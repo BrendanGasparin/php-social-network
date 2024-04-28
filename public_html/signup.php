f0effc353a64b43cd979d72ad3f2d54f0c51b4f9
Brendan Gasparin <brendan.gasparin@gmail.com>
2024-04-29 00:19:40 +1000

    Change all on-screen mentions of Social Network to PHP Social Network

--- public_html/signup.php
+++ public_html/signup.php
@@ -4,13 +4,13 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Social Network</title>
+    <title>PHP Social Network</title>
     <link href="./css/style.css" rel="stylesheet" />
     <link href="./css/userform.css" rel="stylesheet" />
   </head>
   <body>
     <header>
-    <h1>Social Network</h1>
+    <h1>PHP Social Network</h1>
     </header>
     <main>
       <form action="." class="user-form" method="post">
