cfc06a6384048c12d00805fffbbe2743007f6b6b
Brendan Gasparin <brendan.gasparin@gmail.com>
2024-04-29 11:48:46 +1000

    Correct errors in login.inc.php and signup.inc.php

--- public_html/includes/signup.inc.php
+++ public_html/includes/signup.inc.php
@@ -1,6 +1,7 @@
-/*
-    signup.inc.php
-    Author: Brendan Gasparin
-    Date: 29 April 2024
-    Handles sign up form input.
-*/
+<?php
+    /*
+        signup.inc.php
+        Author: Brendan Gasparin
+        Date: 29 April 2024
+        Handles sign up form input.
+    */
