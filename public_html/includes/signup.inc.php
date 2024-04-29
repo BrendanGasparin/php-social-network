8d32aee891ef7ab50b06547f88e3684ee23e48ce
Brendan Gasparin <brendan.gasparin@gmail.com>
2024-04-29 12:06:27 +1000

    Partially implement signup form data collection

--- public_html/includes/signup.inc.php
+++ public_html/includes/signup.inc.php
@@ -5,3 +5,21 @@
         Date: 29 April 2024
         Handles sign up form input.
     */
+
+    if (isset($_POST["submit"])) {
+        // Get the data
+        $firstName = $_POST["firstname"];
+        $lastName = $_POST["lastname"];
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        $password2 = $_POST["password2"];
+        $email = $_POST["email"];
+
+        $day = $_POST["day-picker"];
+        $month = $_POST["month-picker"];
+        $year = $_POST["year-picker"];
+
+        // TODO: Instantiate the SignupController class
+        // TODO: Run error handlers and user signup
+        // TODO: Go back to the home page
+    }
