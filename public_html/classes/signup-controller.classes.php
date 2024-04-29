99f49d94a7f7687f0a3fb453ead58e631a580ba5
Brendan Gasparin <brendan.gasparin@gmail.com>
2024-04-29 13:47:24 +1000

    Set up basic (unimplemented) checks on sign up info

--- public_html/classes/signup-controller.classes.php
+++ public_html/classes/signup-controller.classes.php
@@ -18,4 +18,29 @@
             $this->$password = $password;
             $this->$password2 = $password2;
         }
+
+        private function emptyInput() {
+            if (empty($this->$firstname) || empty($this->$lastname) || empty($this->$username) || empty($this->$email) || empty($this->$firstname) || empty($this->$password || empty($this->$password2)))
+                return true;
+            else
+                return false;
+        }
+
+        /*
+            Allow usernames consisting of alphanumeric characters, periods, and underscores,
+            but the first character must be alphabetic.
+        */
+        private function invalidUsername() {
+            if (!pregmatch("/^[a-zA-Z][a-zA-Z0-9._]*$/", $this->username))
+                return true;
+            else
+                return false;
+        }
+
+        private function invalidEmail() {
+            if (!filter_var($this->email, FILTER_VALIDATE_EMAIL))
+                return false;
+            else
+                return true;
+        }
     }
