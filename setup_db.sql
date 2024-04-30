f35553995bcf2ae58a67d36065ebd9ff815ec0cc
Brendan Gasparin <brendan.gasparin@gmail.com>
2024-04-30 23:15:45 +1000

    Modify setup_db.sql to make it a valid SQL statement

--- setup_db.sql
+++ setup_db.sql
@@ -1,10 +1,10 @@
-CREATE TABLE users {
-    id int(11) AUTO_INCREMENT PRIMARY KEY not null,
+CREATE TABLE users (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY not null,
     firstname TINYTEXT not null,
     lastname TINYTEXT not null,
     username TINYTEXT not null,
     email TINYTEXT not null,
     dob DATE not null,
-    group INT not null,
-    passwordhash LONGTEXT not null
-};
+    user_group INT not null,
+    password_hash LONGTEXT not null
+);
