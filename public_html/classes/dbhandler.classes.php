c87ef7a2df5d56f377dc1b2fed4e0664b2c027ee
Brendan Gasparin <brendan.gasparin@gmail.com>
2024-05-01 00:39:50 +1000

    Fix indentation in dbhandler.classes.php

--- public_html/classes/dbhandler.classes.php
+++ public_html/classes/dbhandler.classes.php
@@ -1,15 +1,13 @@
 <?php
-    require_once '../credentials/credentials.php';
-
-    class DBHandler {
-        private function connect() {
-            try {
-                $dbhandler = new PDO('mysql:host=' . $dbHost . ';dbname=' . $dbName, $username, $password);
-                return $dbhandler;
-            }
-            catch (PDEOException $e) {
-                print "Error: " . $e->getMessage() . "<br />";
-                die();
-            }
+class DBHandler {
+    private function connect() {
+        try {
+            $dbhandler = new PDO('mysql:host=' . $dbHost . ';dbname=' . $dbName, $username, $password);
+            return $dbhandler;
+        }
+        catch (PDEOException $e) {
+            print "Error: " . $e->getMessage() . "<br />";
+            die();
         }
     }
+}
